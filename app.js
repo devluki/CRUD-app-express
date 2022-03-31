10f5588fa09279f9162564202915a3b092f0e99d
devluki <lukasz.zabiegli@gmail.com>
2022-04-01 00:09:53 +0200

    Add api news

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ var indexRouter = require('./routes/index');
 var newsRouter = require('./routes/news');
 var quizRouter = require('./routes/quiz');
 var adminRouter = require('./routes/admin');
+var apiRouter = require('./routes/api');
 
 // mongodb+srv://admin:<password>@cluster0.ktjzp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 // W6BOcG4qtZugZuya
@@ -63,6 +64,7 @@ app.use('/', indexRouter);
 app.use('/news', newsRouter);
 app.use('/quiz', quizRouter);
 app.use('/admin', adminRouter);
+app.use('/api', apiRouter);
 
 
 // catch 404 and forward to error handler
---
+++ routes/api.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const News = require('../models/news');
+const defaultSort = -1;
+
+/* GET home page. */
+router.get('/', (req, res) => {
+    console.log(req.query);
+    const search = req.query.search || '';
+    let sort = req.query.sort || defaultSort;
+
+    if (sort !== -1 || sort !== 1) {
+        sort = defaultSort;
+    }
+
+    const findNews = News.find({
+        title: new RegExp(search.trim(), 'i')
+    }).sort({
+        created: sort
+    });
+
+    findNews.exec((err, data) => {
+        // console.log(data);
+
+
+        res.json(data);
+    })
+
+
+});
+
+
+
+router.get('/:id', (req, res) => {
+
+    const id = req.params.id;
+    const findNews = News.findById(id)
+    // .select('_id  title')
+
+
+    findNews.exec((err, data) => {
+        // console.log(data);
+
+
+        res.json(data);
+    })
+
+
+});
+
+module.exports = router;
