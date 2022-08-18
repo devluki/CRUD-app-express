3b952a4bc9a2ca35d74dc5e9899b5a3dfe0db760
Łukasz <100274711+devluki@users.noreply.github.com>
2022-08-18 13:35:52 +0200

    Delete config.js
    Delete config file

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    db: 'mongodb+srv://admin:W6BOcG4qtZugZuya@cluster0.ktjzp.mongodb.net/test?retryWrites=true&w=majority',
-    keySession: ['123456'],
-    maxAgeSession: 24 * 60 * 60 * 1000
-}
